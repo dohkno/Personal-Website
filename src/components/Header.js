ac69406fde50c88a32352177854209fa16ca80cb
meeej <ms0767467@gmail.com>
2020-04-05 21:57:42 -0700

    removed unused variables

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ export default class Header extends Component {
         if(this.props.data){
             var name = this.props.data.name
             var occupation= this.props.data.occupation;
-            var description= this.props.data.description;
             var city= this.props.data.address.city;
             var networks = this.props.data.social.map((network) => {
                 return <li key={network.name}>
